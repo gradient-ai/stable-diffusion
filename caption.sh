9da7dcee369d5d007bc9e85a3e9dd4fcd96ddb01
root <swimauth>
2023-08-09 23:09:25 +0000

    konhoa directoy

--- caption.sh
+++ caption.sh
@@ -1,12 +1,19 @@
-root_dir="./"
-kohya="/storage/content/kohya-trainer"
-tag_threshold=0.35 #@param {type:"slider", min:0.0, max:1.0, step:0.01}
 images_folder=$1
+echo "images_folder ${images_folder}"
+
+mkdir -p /storage/content
+kohya="/storage/content/kohya-trainer"
+if [[ ! -d "${kohya}" ]]; then
+    git clone https://github.com/swimauth/sd-scripts.git "${kohya}"
+    cd "${kohya}"
+    git reset --hard paperspace
+    cd /notebooks
+fi
+
+tag_threshold=0.35 #@param {type:"slider", min:0.0, max:1.0, step:0.01}
+
 caption_min=10 #@param {type:"number"}
 caption_max=300 #@param {type:"number"}
-
-
-echo  "===${images_folder}"
 
 cd ${kohya}
 
@@ -17,7 +24,6 @@ python finetune/tag_images_by_wd14_tagger.py "${images_folder}/" \
     --batch_size=8 \
     --caption_extension=.tags
  #   --force_download \
-
 
 python finetune/make_captions.py "${images_folder}"\
     --beam_search \
@@ -31,7 +37,6 @@ python finetune/make_captions.py "${images_folder}"\
 python finetune/merge_captions_tag.py "${images_folder}"
 
 cd /notebooks
-
 
 find "${images_folder}" -type f ! \( -name '*.png' -o -name '*.jpg' -o -name '*.jpeg' \) -delete
 rm -r "${images_folder}/.ipynb_checkpoints"
